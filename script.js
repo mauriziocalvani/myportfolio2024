04d46c62e32562042e6cd166804d4936d970807b
mauriziocalvani <42435656+mauriziocalvani@users.noreply.github.com>
2024-01-24 14:54:42 +0100

    bug fix on scroll top

--- script.js
+++ script.js
@@ -91,8 +91,8 @@ $(window).on('load', function(event){
 
 function goToAnchor(elId,margin_top){
     console.log("goToAnchor e elId = " + elId + " e margin_top = " + margin_top);
-    var scrollTopPx= (jQuery(elId).offset().top-margin_top)+'px';
-    console.log("scrollTopPx = " + scrollTopPx)
+    var scrollTopPx= (jQuery(elId).offset().top)+margin_top+'px';
+    console.log("scrollTopPx ok = " + scrollTopPx)
     jQuery('html,body').animate({
         scrollTop: scrollTopPx
     }, 1500, "easeOutQuint");
