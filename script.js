4573bb63343b22f09ef8b7ec7853987722cd7826
mauriziocalvani <42435656+mauriziocalvani@users.noreply.github.com>
2024-01-27 15:45:34 +0100

    fixed bug

--- script.js
+++ script.js
@@ -73,10 +73,10 @@ $(document).ready(function () {
     $("a.open-video-full-screen").on("mouseleave", function (event) {
         event.stopPropagation();
         // play modal video
-        $(".modal-video-container video").delay(500).trigger("play");
+        $(".modal-video-container video").delay(100).trigger("play");
     });
    
-    $("img.close-video-full-screen").on("click", function (event) {
+    $("img.close-video-full-screen").on("mouseleave", function (event) {
         event.stopPropagation();
         // pause modal video
         $(".modal-video-container video").trigger("pause");
